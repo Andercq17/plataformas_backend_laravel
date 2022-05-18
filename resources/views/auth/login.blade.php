6cb327a1dad6c65ede0bae14716f23595d311468
Ander <a.cardenas.quiros@gmail.com>
2022-05-18 17:29:29 -0500

    archivos actualizados

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,8 +22,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <!-- Custom-Files -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <!-- Bootstrap-Core-CSS -->
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-    <!-- Style-CSS -->
+   
     <!-- font-awesome-icons -->
     <link href="css/font-awesome.css" rel="stylesheet">
     <!-- //font-awesome-icons -->
@@ -35,47 +34,71 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
+    <link href="//fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link href="//fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/style-starter.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/btn_wpp.css'); }}">
 
 </head>
 
 <body>
-    <div class="main-sec inner-page">
-        <!-- //header -->
-        <header class="py-sm-3 pt-3 pb-2" id="home">
-            <div class="container">
-                <!-- nav -->
-                <div class="top-w3pvt d-flex">
-                    <div id="logo">
-                        <h1> <a href="index.html"><span class="log-w3pvt">B</span>aggage</a> <label class="sub-des">Online Store</label></h1>
-                    </div>
+<a href="https://wa.me/+573217475876" target="_blank">
+        <img src="https://icons.iconarchive.com/icons/martz90/circle/256/whatsapp-icon.png" id="btn_whatsapp" alt="">
+    </a>
+    <!--header-->
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg stroke">
+                <h1>
+                    <a class="navbar-brand" href="home">
+                        Phot<i class="fas fa-camera"></i>genic
+                    </a>
+                </h1>
+                <!-- if logo is image enable this   
+    <a class="navbar-brand" href="#index.html">
+        <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+    </a> -->
+                <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse"
+                    data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+                </button>
 
-                    <div class="forms ml-auto">
+                @include('menu')
+                <!-- search button -->
+                <div class="search-right ml-lg-3">
+                    <div class="search-container">
+                        <form action="/search" method="get">
+                            <input class="search expandright" id="searchright" type="search" name="q"
+                                placeholder="Search">
+                            <label class="button searchbutton" for="searchright"><i class="fas fa-search"></i></label>
+                        </form>
                     </div>
                 </div>
-                <div class="nav-top-wthree">
-                    <nav>
-                        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
-                        <input type="checkbox" id="drop" />
-                        <ul class="menu"></ul>
-                    </nav>
-                    <!-- //nav -->
-                    <div class="search-form ml-auto">
-                        <div class="form-w3layouts-grid">
-                            <form action="#" method="post" class="newsletter">
-                                <input class="search" type="search" placeholder="Search here..." required="">
-                                <button class="form-control btn" value=""><span class="fa fa-search"></span></button>
-
-                            </form>
+                <!-- //search button -->
+                <!-- toggle switch for light and dark theme -->
+                <div class="cont-ser-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox">
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
                         </div>
-                    </div>
-                    <div class="clearfix"></div>
+                    </nav>
                 </div>
-            </div>
-        </header>
-        <!-- //header -->
-
-    </div>
+                <!-- //toggle switch for light and dark theme -->
+            </nav>
+        </div>
+    </header>
+    <!--//header-->
 
     <div id="ventana" class="ligthbox hidden">
         <div class="contenido-ventana">
@@ -91,9 +114,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <section class="banner-bottom py-5">
         <div class="container">
             <div class="content-grid">
-                <div class="text-center icon">
-                    <span class="fa fa-unlock-alt"></span>
-                </div>
+   
                 <div class="content-bottom">
                     
 
@@ -144,6 +165,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                                 </a>
                             @endif
                             @if (Route::has('register'))
+                            
                                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                                     {{ __('Register with us!') }}
                                 </a>
@@ -161,123 +183,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         </div>
     </section>
     <!-- /login -->
-    <!--/shipping-->
-    <section class="shipping-wthree">
-        <div class="shiopping-grids d-lg-flex">
-            <div class="col-lg-4 shiopping-w3pvt-gd text-center">
-                <div class="icon-gd"><span class="fa fa-truck" aria-hidden="true"></span>
-                </div>
-                <div class="icon-gd-info">
-                    <h3>FREE SHIPPING</h3>
-                    <p>On all order over $2000</p>
-                </div>
-            </div>
-            <div class="col-lg-4 shiopping-w3pvt-gd sec text-center">
-                <div class="icon-gd"><span class="fa fa-bullhorn" aria-hidden="true"></span></div>
-                <div class="icon-gd-info">
-                    <h3>FREE RETURN</h3>
-                    <p>On 1st exchange in 30 days</p>
-                </div>
-            </div>
-            <div class="col-lg-4 shiopping-w3pvt-gd text-center">
-                <div class="icon-gd"> <span class="fa fa-gift" aria-hidden="true"></span></div>
-                <div class="icon-gd-info">
-                    <h3>MEMBER DISCOUNT</h3>
-                    <p>Register &amp; save up to $29%</p>
-                </div>
-
-            </div>
-        </div>
-
-    </section>
-    <!--//shipping-->
-    <!-- footer -->
-    <div class="footer_agileinfo_topf py-5">
-        <div class="container py-md-5">
-            <div class="row">
-                <div class="col-lg-3 footer_wthree_gridf mt-lg-5">
-                    <h2><a href="index.html"><span>B</span>aggage
-                        </a> </h2>
-                    <label class="sub-des2">Online Store</label>
-                </div>
-                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-4">
-                    <ul class="footer_wthree_gridf_list">
-                        <li>
-                            <a href="index.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Home</a>
-                        </li>
-                        <li>
-                            <a href="about.html"><span class="fa fa-angle-right" aria-hidden="true"></span> About</a>
-                        </li>
-                        <li>
-                            <a href="shop.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Shop</a>
-                        </li>
-                        <li>
-                            <a href="shop.html"><span class="fa fa-angle-right" aria-hidden="true"></span>Collections</a>
-                        </li>
-
-                    </ul>
-                </div>
-                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-sm-4 mt-3">
-                    <ul class="footer_wthree_gridf_list">
-                        <li>
-                            <a href="single.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Extra Page</a>
-                        </li>
-
-                        <li>
-                            <a href="#"><span class="fa fa-angle-right" aria-hidden="true"></span> Terms & Conditions</a>
-                        </li>
-                        <li>
-                            <a href="single.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Shop Single</a>
-                        </li>
-                        <li>
-                            <a href="contact.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-sm-4 mt-3">
-                    <ul class="footer_wthree_gridf_list">
-                        <li>
-                            <a href="login.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Login </a>
-                        </li>
-
-                        <li>
-                            <a href="register.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Registro</a>
-                        </li>
-                        <li>
-                            <a href="#"><span class="fa fa-angle-right" aria-hidden="true"></span>Privacy & Policy</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-
-            <div class="w3ls-fsocial-grid hidden">
-                <h3 class="sub-w3ls-headf">Follow Us</h3>
-                <div class="social-ficons">
-                    <ul>
-                        <li><a href="#"><span class="fa fa-facebook"></span> Facebook</a></li>
-                        <li><a href="#"><span class="fa fa-twitter"></span> Twitter</a></li>
-                        <li><a href="#"><span class="fa fa-google"></span>Google</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="move-top text-center mt-lg-4 mt-3">
-                <a href="#home"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
-            </div>
-        </div>
-    </div>
-    <!-- //footer -->
-
-    <!-- copyright -->
-    <div class="cpy-right text-center py-3">
-        <p>© 2019 Baggage. All rights reserved | Design by
-            <a href="http://w3layouts.com"> W3layouts.</a>
-        </p>
-
-    </div>
-    <!-- //copyright -->
+    @include('footer')
 
 </body>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title></title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="" />
+    <!--script src="js/main.js"></script>
+    <script src="js/md5.min.js"></script>
+    <script src="js/login.js"></script-->
+    <!-- //Meta tag Keywords -->
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Bootstrap-Core-CSS -->
+   
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <!-- /Fonts -->
+    <link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+    <!-- //Fonts -->
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link href="//fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link href="//fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/style-starter.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/btn_wpp.css'); }}">
+
+</head>
+
+<body>
+<a href="https://wa.me/+573217475876" target="_blank">
+        <img src="https://icons.iconarchive.com/icons/martz90/circle/256/whatsapp-icon.png" id="btn_whatsapp" alt="">
+    </a>
+    <!--header-->
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg stroke">
+                <h1>
+                    <a class="navbar-brand" href="home">
+                        Phot<i class="fas fa-camera"></i>genic
+                    </a>
+                </h1>
+                <!-- if logo is image enable this   
+    <a class="navbar-brand" href="#index.html">
+        <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+    </a> -->
+                <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse"
+                    data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+                </button>
+
+                @include('menu')
+                <!-- search button -->
+                <div class="search-right ml-lg-3">
+                    <div class="search-container">
+                        <form action="/search" method="get">
+                            <input class="search expandright" id="searchright" type="search" name="q"
+                                placeholder="Search">
+                            <label class="button searchbutton" for="searchright"><i class="fas fa-search"></i></label>
+                        </form>
+                    </div>
+                </div>
+                <!-- //search button -->
+                <!-- toggle switch for light and dark theme -->
+                <div class="cont-ser-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox">
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
+                        </div>
+                    </nav>
+                </div>
+                <!-- //toggle switch for light and dark theme -->
+            </nav>
+        </div>
+    </header>
+    <!--//header-->
+
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -57,3 +152,9 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+
+@include('footer')
+
+</body>
+
+</html>
